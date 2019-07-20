cdb0f3a0f2b4b61cd722451e492e195017e0aab2
tungvt45 <tungvt2167@gmail.com>
2019-07-20 23:39:34 +0700

    update api

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://192.168.1.102:8080";
+export const API_URL = "https://badmintonsystem.herokuapp.com";
 export const DJISYMBOL = "&symbol=DJI";
-export const URL = "https://badmintonsystem.herokuapp.com";
+export const URL = "https://userwebbadminton.herokuapp.com";
